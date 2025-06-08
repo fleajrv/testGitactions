be5c0311b3bcfa35ca4d65b3686e981a3c93ce10
fleajrv <reyesalejandro162003@gmail.com>
2025-06-08 19:05:46 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,23 +1,7 @@
 def func1():
     print("bien")
 
+
 def func2():
-        print("mall")
+    print("mall")
 
-"""
-def me_gusta_el_cafe():
-        print("chao")
-
-func1()
-
-def funcionclasica():
-    print("hola")
-                print("adios")
-
-
-
-
-
-
-                                                                                    me_gusta_el_cafe()
-"""
